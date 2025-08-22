b71b422276ba41dd3bc1e3718ff580869f1af150
YasH0ShirsatH <yashshirsath.datagrid@gmail.com>
2025-08-22 15:18:27 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- An unexamined life is not worth living. - Socrates -->
+    
+hi <br>
+this is page no.  {{ $id }} , name : {{ $name }}
+</div>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::get('/about',array('as'=>'about.page' , function () {
+    $ab = route('about.page');
+    return view('about',['ab'=> $ab]);
+}));
+Route::get('/home/{id}/{name}', function ($id,$name) {
+    
+    return view('home',['id' => $id , 'name'=> $name]);
+});
+
+Route::get('/info/{aba}',[UserController::class,'index']);
+
+
+
+
+
+
+
+
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
