e2617b95d582dc3f9bdb4710319a8df638135da6
YasH0ShirsatH <yashshirsath.datagrid@gmail.com>
2025-08-22 17:14:12 +0530

    This reverts commit a57e7a237e3e32bb6e8d7b35afd2cdcc832f921d.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,6 @@
-<!-- <div>
- An unexamined life is not worth living. - Socrates 
+<div>
+    <!-- An unexamined life is not worth living. - Socrates -->
     
 hi <br>
 this is page no.  {{ $id }} , name : {{ $name }}
-</div> -->
-
-@extends('layout.app')
-
-@section('content')
-    <h1>
-        home page
-    </h1>
-@endsection
+</div>
